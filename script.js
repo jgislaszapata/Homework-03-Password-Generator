b4afa444d15dec0e1fdbd9ed4d3bd77d16c70a74
Jesus Islas <jesusislas96@gmail.com>
2022-05-12 09:29:16 -0400

    application prompts for password length and throws alert if password is too long or too short.

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var promptOne = prompt("How many characters would you like password to be?");
+  if (promptOne < 8 || promptOne > 128) {
+    alert('Invalid length. Must be between 8 and 128 characters.');
+  }
 
-  passwordText.value = password;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
