017e46512e60600e27b8200e72892d3df2384e9c
Jesus Islas <jesusislas96@gmail.com>
2022-05-16 18:23:12 -0400

    working application complete.

--- script.js
+++ script.js
@@ -3,43 +3,67 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
- var promptLength = prompt("Enter your desired password character length.");
-   if (promptLength < 8 || promptLength > 128 || isNaN(promptLength)) {
-     alert("Invalid entry. You must select a number between 8 and 128.");
-   } else {
-     alert(`Your password will be ${promptLength} characters long.`);
-     confirmLower();
-   }
+    var promptLength = prompt("Enter your desired password length.");
+    if (promptLength < 8 || promptLength > 128 || isNaN(promptLength)) {
+        alert("Invalid entry. Please enter a number between 8 and 128.");
+        return; 
+    }
+    console.log(promptLength);
 
-  function confirmLower() {
-    console.log("confirm message");
-  }
-  }
+    var listChar = '';
+    var pass = '';
+    var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+    var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numChar = '1234567890';
+    var specialChar = `!"#$%&'()*+,-./:;<=>?@[\\]^_{|}~`;
 
+    var confirmLower = confirm('Would you like your password to include lowercase letters?');
+    if (confirmLower === true) {
+      listChar += lowerChar;
+    }
+    console.log(confirmLower);
+    console.log(listChar);
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+    var confirmUpper = confirm('Would you like your password to include uppercase letters?');
+    if (confirmUpper === true) {
+      listChar += upperChar;
+    }
+    console.log(confirmUpper);
+    console.log(listChar);
 
-  // passwordText.value = password;
+    var confirmNum = confirm('Would you like your password to include numbers?');
+    if (confirmNum === true) {
+      listChar += numChar; 
+    }
+    console.log(confirmNum);
+    console.log(listChar);
 
+    var confirmSpecial = confirm('Would you like your password to include special characters?');
+    if (confirmSpecial === true) {
+      listChar += specialChar;
+    }
+    console.log(confirmSpecial);
+    console.log(listChar);
 
+    for (var i = 0; i<promptLength; i++) {
+      var index = Math.floor(Math.random() * listChar.length);
+      pass += listChar.substring(index, index+1);
+    }
+    console.log(pass);
+    alert("Your randomly generated password is: " + pass)
+    
+    
+    
+    
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-//  // var promptLength = prompt("How many characters would you like password to be?");
-//   // if (promptLength < 8 || promptLength > 128) {
-//   //   alert("Invalid length. Must be between 8 and 128 characters.");
-//   // } else {
-//   //   confirmLower();
-//   // }
-
-//   // function confirmLower() {
-//   //   var confirmAnswer = confirm("Allow both lower and uppercase letters in password?");
-//   // }
-
 
 
 
